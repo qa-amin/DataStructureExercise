65bcea5eac0e858b2234543901a56d3232bc5f40
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-01-29 16:30:50 +0330

    First repeating character

---
+++ 1.50_popular_coding_interview_problems/2.First_repeating_character/First_repeating/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace First_repeating
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Time : O(n) Space : O(n)
+			string str = "responsible";
+
+			var dic = new Dictionary<char, bool>();
+
+			char? answer = null;
+			
+			foreach (var s in str)
+			{
+				if (dic.ContainsKey(s))
+				{
+					answer = s;
+					break;
+				}
+				else
+				{
+					dic[s] = true;
+				}
+			}
+
+			if (answer != null)
+			{
+				Console.WriteLine($"First Repeating Character is {answer}");
+			}
+			else
+			{
+				Console.WriteLine("There is No Repeating Character");
+			}
+		}
+	}
+}
