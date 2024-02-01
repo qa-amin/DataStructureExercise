2ff2753302f3b10746d2fb4cc51fa9d7928f1958
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-01 22:37:37 +0330

    Tree depth first search

---
+++ 1.50_popular_coding_interview_problems/5.Tree_depth_first_search/depth_first_search/BinaryTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace depth_first_search
+{
+	public class BinaryTree
+	{
+		public int Data { get; set; }
+		public BinaryTree Left { get; set; }
+		public BinaryTree Right { get; set; }
+	}
+}
---
+++ 1.50_popular_coding_interview_problems/5.Tree_depth_first_search/depth_first_search/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace depth_first_search
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var root = new BinaryTree();
+
+			root.Data = 1;
+
+			root.Left = new BinaryTree() ;
+			root.Left.Data = 2;
+
+			root.Left.Left = new BinaryTree();
+			root.Left.Left.Data = 3;
+
+			root.Left.Right = new BinaryTree();
+			root.Left.Right.Data = 4;
+
+			root.Right = new BinaryTree();
+			root.Right.Data = 5;
+
+			root.Right.Left = new BinaryTree();
+			root.Right.Left.Data = 6;
+
+			root.Right.Right = new BinaryTree();
+			root.Right.Right.Data = 7;
+
+			Console.WriteLine("Pre Order Print is : ");
+			PreOrder(root);
+
+			Console.WriteLine("********************************");
+
+			Console.WriteLine("In Order Print is : ");
+			InOrder(root);
+
+			Console.WriteLine("********************************");
+
+			Console.WriteLine("Post Order Print is : ");
+			PostOrder(root);
+		}
+
+		public static void PreOrder(BinaryTree root)
+		{
+			if (root != null)
+			{
+				Console.WriteLine(root.Data);
+
+				PreOrder(root.Left);
+				PreOrder(root.Right);
+			}
+				
+		}
+
+		public static void InOrder(BinaryTree root)
+		{
+			if (root != null)
+			{
+				InOrder(root.Left);
+
+				Console.WriteLine(root.Data);
+				InOrder(root.Right);
+			}
+		}
+
+		public static void PostOrder(BinaryTree root)
+		{
+			if (root != null)
+			{
+				PostOrder(root.Left);
+				PostOrder(root.Right);
+
+				Console.WriteLine(root.Data);
+			}
+		}
+	}
+}
