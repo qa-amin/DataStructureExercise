651b375351ba5925bff020998060318465e2532d
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-02 22:02:02 +0330

    Maximum subarray

---
+++ 1.50_popular_coding_interview_problems/6.Maximum_subarray/Maximum_subarray/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Maximum_subarray
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = { 2, 3, -8, 4, 5 };
+
+			int localSum = 0;
+			int globalSum = arr[0];
+
+			for (int i = 0; i < arr.Length; i++)
+			{
+				localSum = Math.Max(arr[i], arr[i] + localSum);
+				globalSum = Math.Max(globalSum,localSum);
+			}
+
+			Console.WriteLine($"Max Sub array is : {globalSum}");
+		}
+	}
+}
