5d3b43a01efd2978a7e6d47831d69c38402f7687
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-03 14:08:44 +0330

    Reverse Binary Tree

---
+++ 1.50_popular_coding_interview_problems/7.Reverse_binary_tree/Reverse_binary_tree/BinaryTree.cs
@@ -0,0 +1,9 @@
+﻿namespace Reverse_binary_tree
+{
+	public class BinaryTree
+	{
+		public int Data { get; set; }
+		public BinaryTree Left { get; set; }
+		public BinaryTree Right { get; set; }
+	}
+}
---
+++ 1.50_popular_coding_interview_problems/7.Reverse_binary_tree/Reverse_binary_tree/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Reverse_binary_tree
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var root = new BinaryTree();
+
+			root.Data = 1;
+
+			root.Left = new BinaryTree();
+			root.Left.Data = 2;
+
+			root.Left.Left = new BinaryTree();
+			root.Left.Left.Data = 3;
+
+			root.Left.Right = new BinaryTree();
+			root.Left.Right.Data = 4;
+
+			root.Right = new BinaryTree();
+			root.Right.Data = 5;
+
+			root.Right.Left = new BinaryTree();
+			root.Right.Left.Data = 6;
+
+			root.Right.Right = new BinaryTree();
+			root.Right.Right.Data = 7;
+
+
+			ReverseBinaryTree(root);
+
+			
+		}
+
+		public static void ReverseBinaryTree(BinaryTree root)
+		{
+			if (root != null)
+			{
+				// Swap
+
+				(root.Left, root.Right) = (root.Right, root.Left);
+
+				ReverseBinaryTree(root.Left);
+				ReverseBinaryTree(root.Right);
+			}
+		}
+
+		
+	}
+}
