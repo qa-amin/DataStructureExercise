06c535678f74780ae640629b67f2fa5a3b201c77
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-06 23:49:41 +0330

    Peak finding

---
+++ 1.50_popular_coding_interview_problems/10.Peak_finding/Peak_finding/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Peak_finding
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = { 1, 3, 9, 10, 5, 2, 6, 7, 4 };
+
+			int left = 0;
+			int right = arr.Length - 1;
+			int mid;
+
+			while (left < right)
+			{
+				mid = (left + right) / 2;
+				if (arr[mid] < arr[mid + 1])
+				{
+					left = mid + 1;
+				}
+				else
+				{
+					right = mid;
+				}
+
+				
+			}
+
+			Console.WriteLine($"Peak is {arr[left]} and Index of Peak is {left}");
+		}
+	}
+}
