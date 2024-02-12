2841bc59a70633b9cc9f6f6b94cf78020e6b24f8
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-12 23:56:03 +0330

    Longest substring without repeating characters

---
+++ 1.50_popular_coding_interview_problems/8.Longest_substring/Longest_substring/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Longest_substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "abcdbeghef";
+
+            int maxLength = 0;
+
+            int start = 0;
+
+            int[] indexes = new int[128];
+
+            for (int i = 0; i < indexes.Length; i++)
+            {
+                indexes[i] = -1;
+            }
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                var asci = Convert.ToByte(str[i]);
+                if (indexes[asci] >= start)
+                {
+                    start = indexes[asci] + 1;
+                }
+
+                indexes[asci] = i;
+
+                maxLength = Math.Max(maxLength, i - start + 1);
+            }
+
+            Console.WriteLine($"Longest substring without repeating characters is {maxLength}");
+        }
+    }
+}
